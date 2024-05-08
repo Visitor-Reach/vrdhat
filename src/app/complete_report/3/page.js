0fdcdf088b29a03b5a3feaaaad1f9dc2c6fee947
alex <alexanderlopezp@gmail.com>
2024-05-08 15:18:28 -0500

    Fix graphic issues

--- src/app/complete_report/3/page.js
+++ src/app/complete_report/3/page.js
@@ -83,69 +83,68 @@ export default function CompleteReportPage3() {
                         width={400}
                     />
                 </div>
-                <div className="relative col-span-2 menu-shadow">
+                <div className="relative col-span-2 row-span-2 menu-shadow">
                     <div>
                         <NavBar user_key={user_key} map={map} page={3} ></NavBar>
                     </div>
-                    
-                    
                 </div>
-                <div className='col-span-4 flex flex-col justify-center justify-items-center bg-[#F9FCFD] py-[90px] px-[130px] '>
-                    <div className="bg-white w-[797px] h-[430px] rounded-3xl m-auto card-shadow">
-                        <div className="relative m-auto w-[120px] top-10">
-                            <Image
-                                src={"/homepod.svg"}
-                                alt={" "}
-                                height={400}
-                                width={400}
-                            />
-                        </div>
-                        <h1 className='font-medium text-[32px] text-[#050938] w-full text-center m-auto relative top-20 leading-[115%] tracking-[-1.28px]'>What Is <a className="text-[#0179FF]">Voice Search</a> & Why It’s Important</h1>
-                        <h2 className='font-medium text-[20px] text-[#75778B] w-[658px] text-center m-auto relative top-24 leading-[143%] tracking-[-0.8px]'>
-                            Voice technology allows people to perform a hands-free search by asking questions to their smart devices such as smartphones, smart speakers, and in-car systems. Your church’s Digital Voice Score shows how optimized your digital presence is when it comes to showing up in voice search results.
-                        </h2>
-                    </div>
-                    <div className="bg-white w-[797px] h-[430px] rounded-3xl m-auto card-shadow bg-[url('/woman-background.png')] bg-cover overflow-hidden">
-                        <div className='w-[797px] h-[430px] pt-[100px] page-3-box-gradient'>
-                                <h1 className='relative text-white w-2/3 ml-10 text-[80px] font-bold'>57%</h1>
-                                <h2 className='relative text-white w-[253px] ml-10 text-[20px] leading-[143%] tracking-[-0.8px]'>of American adults use voice assistants on their devices to find out information on a daily basis.</h2>
-                                <h3 className='relative text-white w-2/3 ml-10 mt-[40px] text-[14px]'>Source: NPR</h3>
-                        </div>
-                        
-                    </div>
+                <div className='col-span-7 row-span-2 bg-[#F9FCFD] pt-[90px] pl-[130px] overflow-hidden '>
+                    <div className='flex'>
+                        <div className='flex flex-col'>
+                            
+                            <div className="bg-white w-[797px] h-[430px] rounded-3xl m-auto card-shadow py-[35px]">
+                                <div className="m-auto w-[120px]">
+                                    <Image
+                                        src={"/homepod.svg"}
+                                        alt={" "}
+                                        height={400}
+                                        width={400}
+                                    />
+                                </div>
+                                <h1 className='mt-[16px] font-medium text-[32px] text-[#050938] w-full text-center m-auto leading-[115%] tracking-[-1.28px]'>What Is <a className="text-[#0179FF]">Voice Search</a> & Why It’s Important</h1>
+                                <h2 className='mt-[16px] font-medium text-[20px] text-[#75778B] w-[658px] text-center m-auto leading-[143%] tracking-[-0.8px]'>
+                                    Voice technology allows people to perform a hands-free search by asking questions to their smart devices such as smartphones, smart speakers, and in-car systems. Your church’s Digital Voice Score shows how optimized your digital presence is when it comes to showing up in voice search results.
+                                </h2>
+                            </div>
 
-                    
-                </div>
-                <div className='col-span-3 flex flex-col justify-center items-center bg-[#F9FCFD]'>
-                    <div className=" w-[453px] h-[880px] rounded-3xl ml-[0px] py-[66px] vertical-card-shadow">
-                        <h1 className='text-[#050938] w-[353px] m-auto text-center font-[570] text-[32px] justify-center justify-items-center leading-[115%] tracking-[-1.3px]'>Your Digital Voice Score</h1>
-                        <div className='m-auto grid justify-center mt-[30px]'>
-                            <Circularbar value={digitalVoice} title={undefined} max_value={250}/>
-                        </div>
-                        <div className="m-auto grid justify-center mt-[50px] w-[110px]">
-                            <Image
-                                src={"/2p_church.svg"}
-                                alt={" "}
-                                height={400}
-                                width={400}
-                            />
-                        </div>
-                        <h1 className='text-[#050938] text-[24px] font-medium m-auto grid justify-center mt-[40px] leading-[115%] tracking-[-0.9px]'>Only 2% of churches</h1>
-                        <h2 className='text-[#75778B] text-[20px] font-regular m-auto grid justify-center w-5/6 mt-[40px] leading-[143%] tracking-[-0.8px] text-center'>
-                            are optimized for voice search. If your church’s digital presence isn’t optimized for voice search, people won’t be able to find and visit your church!
-                        </h2>
-                        
-                        <Link href={{pathname:"https://www.visitorreach.com"}}>
-                            <h3 className='text-[#0179FF] text-[12px] font-regular m-auto grid justify-center w-4/6 mt-[40px] pb-10'>
-                                Source: VisitorReach
-                            </h3>
-                        </Link>
-                        
+                            <div className="bg-white w-[797px] h-[430px] rounded-3xl m-auto card-shadow bg-[url('/woman-background.png')] bg-cover overflow-hidden">
+                                <div className='w-[797px] h-[430px] pt-[100px] page-3-box-gradient'>
+                                        <h1 className='text-white w-2/3 ml-10 text-[80px] font-bold'>57%</h1>
+                                        <h2 className='text-white w-[253px] ml-10 text-[20px] leading-[143%] tracking-[-0.8px]'>of American adults use voice assistants on their devices to find out information on a daily basis.</h2>
+                                        <h3 className='text-white w-2/3 ml-10 mt-[40px] text-[14px]'>Source: NPR</h3>
+                                </div>
+                            </div>
 
+                        </div>
+                        <div className='flex ml-[20px]'>
+                            <div className=" w-[453px] h-[880px] rounded-3xl ml-[0px] py-[66px] vertical-card-shadow">
+                                <h1 className='text-[#050938] w-[353px] m-auto text-center font-[570] text-[32px] justify-center justify-items-center leading-[115%] tracking-[-1.3px]'>Your Digital Voice Score</h1>
+                                <div className='m-auto grid justify-center mt-[30px]'>
+                                    <Circularbar value={digitalVoice} title={undefined} max_value={250}/>
+                                </div>
+                                <div className="m-auto grid justify-center mt-[50px] w-[110px]">
+                                    <Image
+                                        src={"/2p_church.svg"}
+                                        alt={" "}
+                                        height={400}
+                                        width={400}
+                                    />
+                                </div>
+                                <h1 className='text-[#050938] text-[24px] font-medium m-auto grid justify-center mt-[40px] leading-[115%] tracking-[-0.9px]'>Only 2% of churches</h1>
+                                <h2 className='text-[#75778B] text-[20px] font-regular m-auto grid justify-center w-5/6 mt-[40px] leading-[143%] tracking-[-0.8px] text-center'>
+                                    are optimized for voice search. If your church’s digital presence isn’t optimized for voice search, people won’t be able to find and visit your church!
+                                </h2>
+                                
+                                <Link href={{pathname:"https://www.visitorreach.com"}}>
+                                    <h3 className='text-[#0179FF] text-[12px] font-regular m-auto grid justify-center w-4/6 mt-[40px] pb-10'>
+                                        Source: VisitorReach
+                                    </h3>
+                                </Link>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-
         </div>
     )
 }
