29681c55859cfeb4aab669df34f25af9f2ea0685
Glenn Ray <glenn@visitorreach.com>
2024-05-15 17:38:17 -0700

    Fix layout merge issue

--- src/app/user_report/page.js
+++ src/app/user_report/page.js
@@ -35,7 +35,7 @@ export default function SimpleResult() {
         const myHeaders = new Headers()
         myHeaders.append('Content-Type', 'application/json')
         myHeaders.append('Access-Control-Allow-Origin', '*')
-        const response = await fetch(process.env.NEXT_PUBLIC_API_ROOT + '/api/fetch-data', {
+        const response = await fetch('http://localhost:8080/api/fetch-data', {
           method: 'POST',
           body: JSON.stringify({ user_key: user_key }),
           headers: myHeaders,
